f87676b24b15b74c10539797a50d5ff1180e0035
Sreerenj Gopimohanan <Sreerenj.Gopimohanan@heatsoftware.com>
2018-11-04 09:22:34 +0530

    Upgraded to Angular 5

--- BGMPharmaWebApp/Startup.cs
+++ BGMPharmaWebApp/Startup.cs
@@ -41,7 +41,15 @@ namespace BGMPharmaWebApp
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = (context) =>
+                    {
+                        context.Context.Response.Headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
+                        context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
+                        context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+                    }
+            });
 
             app.UseMvc(routes =>
             {
